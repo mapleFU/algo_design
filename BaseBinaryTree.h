d499e2e0c59b7e1a9c4385d4274eed9010975ca6
mapleFU <1506118561@qq.com>
2017-03-29 14:49:38 +0800

    Define some functions outside the class TreeNode

--- BaseBinaryTree.h
+++ BaseBinaryTree.h
@@ -19,7 +19,7 @@ TreeNode<T>* _Value_Insert(const T& Ival, TreeNode<T>* iroot);
 template <typename T>
 struct TreeNode {
     friend class BaseBinaryTree<T>;
-
+    friend class avlTree<T>;
     //Data
     TreeNode<T> *right = nullptr;
     TreeNode<T> *left = nullptr;
@@ -27,56 +27,14 @@ struct TreeNode {
     int times = 1;
     //Data of AvlTree
     int height = 0;
-
     TreeNode() = delete;        //不希望外部有直接定义的
     TreeNode(const T& Ival): val(Ival) {}
-    TreeNode<T>* Destroy(TreeNode<T>* iroot) {
-        if (iroot == nullptr)
-            return iroot;
-        Destroy(iroot->right);
-        Destroy(iroot->left);
-        delete iroot;           //注意要删除iroot, 而不是this
-        return nullptr;
-    }
-    TreeNode<T>* nodeFindMax(TreeNode<T>* iroot) {
-        if (iroot == nullptr)
-            return nullptr;
-        while (iroot->right)
-            iroot = iroot->right;
-        return iroot;
-    }
-
-    TreeNode<T>* nodeFindMin(TreeNode<T>* iroot) {
-        if (iroot == nullptr)
-            return nullptr;
-        while (iroot->left)
-            iroot = iroot->left;
-        return iroot;
-    }
-    void nodeInTraverse(TreeNode<T>* iroot) {
-        if (!iroot)
-            return;
-        nodeInTraverse(iroot->left);
-        for (int i = 0; i < iroot->times; ++i)
-            std::cout<<iroot->val<<' ';
-        nodeInTraverse(iroot->right);
-    }
-    void nodePreTraverse(TreeNode<T>* iroot) {
-        if (!iroot)
-            return;
-        for (int i = 0; i < iroot->times; ++i)
-            std::cout<<iroot->val<<' ';
-        nodePreTraverse(iroot->left);
-        nodePreTraverse(iroot->right);
-    }
-    void nodePostTraverse(TreeNode<T>* iroot) {
-        if (!iroot)
-            return;
-        for (int i = 0; i < iroot->times; ++i)
-            std::cout<<iroot->val<<' ';
-        nodePostTraverse(iroot->left);
-        nodePostTraverse(iroot->right);
-    }
+    TreeNode<T>* Destroy(TreeNode<T>* iroot);
+    inline TreeNode<T>* nodeFindMax(TreeNode<T>* iroot);
+    TreeNode<T>* nodeFindMin(TreeNode<T>* iroot);
+    void nodeInTraverse(TreeNode<T>* iroot);
+    void nodePreTraverse(TreeNode<T>* iroot);
+    void nodePostTraverse(TreeNode<T>* iroot);
 };
 
 
@@ -105,14 +63,100 @@ protected:
     TreeNode<T>* root;          //根节点
     TreeNode<T>* FindMin(TreeNode<T>* iroot);
     TreeNode<T>* FindMax(TreeNode<T>* iroot);
-    TreeNode<T>* clear(TreeNode<T>* pt) {
-        if (pt) {
-            clear(pt->left);
-            clear(pt->right);
-            delete pt;
-        }
-        return nullptr;
-    }
+    TreeNode<T>* clear(TreeNode<T>* pt);
 };
 
+
+template <typename T>
+TreeNode<T> *BaseBinaryTree<T>::FindMin(TreeNode<T> *iroot) {
+    return iroot->nodeFindMin(iroot);
+}
+
+template <typename T>
+TreeNode<T> *BaseBinaryTree<T>::FindMax(TreeNode<T> *iroot) {
+    return iroot->nodeFindMax(iroot);
+}
+
+template <typename T>
+TreeNode<T> *BaseBinaryTree<T>::Find(TreeNode<T> *iroot, const T &to_find) {
+    if (iroot == nullptr || to_find == iroot->val)
+        return iroot;
+    else if (iroot->val < to_find)
+        return Find(iroot->right, to_find);
+    else
+        return Find(iroot->left, to_find);
+}
+
+template <typename T>
+TreeNode<T>* BaseBinaryTree<T>::clear(TreeNode<T>* pt) {
+    if (pt) {
+        clear(pt->left);
+        clear(pt->right);
+        delete pt;
+    }
+    return nullptr;
+}
+
+
+
+
+
+// functions of TreeNode
+template <typename T>
+TreeNode<T>* TreeNode<T>::Destroy(TreeNode<T>* iroot) {
+    if (iroot == nullptr)
+        return iroot;
+    Destroy(iroot->right);
+    Destroy(iroot->left);
+    delete iroot;           //注意要删除iroot, 而不是this
+    return nullptr;
+}
+
+template <typename T>
+TreeNode<T>* TreeNode<T>::nodeFindMax(TreeNode<T>* iroot) {
+    if (iroot == nullptr)
+        return nullptr;
+    while (iroot->right)
+        iroot = iroot->right;
+    return iroot;
+}
+
+template <typename T>
+TreeNode<T>* TreeNode<T>::nodeFindMin(TreeNode<T>* iroot) {
+    if (iroot == nullptr)
+        return nullptr;
+    while (iroot->left)
+        iroot = iroot->left;
+    return iroot;
+}
+
+template <typename T>
+void TreeNode<T>::nodeInTraverse(TreeNode<T>* iroot) {
+    if (!iroot)
+        return;
+    nodeInTraverse(iroot->left);
+    for (int i = 0; i < iroot->times; ++i)
+        std::cout<<iroot->val<<' ';
+    nodeInTraverse(iroot->right);
+}
+
+template <typename T>
+void TreeNode<T>::nodePreTraverse(TreeNode<T>* iroot) {
+    if (!iroot)
+        return;
+    for (int i = 0; i < iroot->times; ++i)
+        std::cout<<iroot->val<<' ';
+    nodePreTraverse(iroot->left);
+    nodePreTraverse(iroot->right);
+}
+
+template <typename T>
+void TreeNode<T>::nodePostTraverse(TreeNode<T>* iroot) {
+    if (!iroot)
+        return;
+    for (int i = 0; i < iroot->times; ++i)
+        std::cout<<iroot->val<<' ';
+    nodePostTraverse(iroot->left);
+    nodePostTraverse(iroot->right);
+}
 #endif //BINARYSEARCHTREE_BASEBINARYTREE_H
