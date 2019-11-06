1d5f453d468ce58f0200223a07d468364d2cd479
mapleFU <1506118561@qq.com>
2019-11-07 02:09:29 +0800

    [Finish] Finish a basic vector with iteratot

--- NaiveDSA/Vector/rust/src/lib.rs
+++ NaiveDSA/Vector/rust/src/lib.rs
@@ -2,8 +2,10 @@
 
 extern crate alloc;
 
-#[allow(unused)]
 pub mod mvector;
+
+#[allow(unused)]
+pub mod raw_vec;
 
 #[cfg(test)]
 mod tests {
@@ -12,9 +14,38 @@ mod tests {
     #[test]
     fn vec_push_back() {
         let mut v: MVector<usize> = MVector::new();
-        for i in 1..2 {
+        assert_eq!(v.pop(), None);
+        for i in 1..=50 {
             v.push(i);
             assert_eq!(v.len(), i);
+        }
+
+        for i in 0..25_usize {
+            assert_eq!(v.pop(), Some(50 - i));
+        }
+    }
+
+    #[test]
+    fn test_insert() {
+        let mut v: MVector<usize> = MVector::new();
+        assert_eq!(v.pop(), None);
+        for i in 1..=50 {
+            v.push(i);
+            assert_eq!(v.len(), i);
+        }
+
+        v.insert(5, 500);
+        assert_eq!(v.get(5), Some(&500_usize));
+        assert_eq!(v.len(), 51);
+
+        let mut cnt = 1;
+        for r in v.into_iter() {
+            if r < 100 {
+                assert_eq!(r, cnt);
+                cnt += 1;
+            } else {
+                assert_eq!(r, 500);
+            }
         }
     }
 }
--- NaiveDSA/Vector/rust/src/mvector.rs
+++ NaiveDSA/Vector/rust/src/mvector.rs
@@ -2,6 +2,10 @@ use std::alloc::{alloc, dealloc, realloc, Layout};
 use std::mem;
 use std::ops::{Deref, DerefMut};
 use std::ptr::Unique;
+
+#[allow(unused)]
+use crate::raw_vec::*;
+
 ///
 /// 按照 《Rust 死灵书》写的 sample
 /// 用到的材料除了 《Rust 死灵书》还有：
@@ -69,7 +73,8 @@ impl<T> MVector<T> {
         if self.len == 0 {
             None
         } else {
-            let to_return = unsafe { Some(ptr::read(self.ptr.as_ptr().offset(self.len as isize - 1))) };
+            let to_return =
+                unsafe { Some(ptr::read(self.ptr.as_ptr().offset(self.len as isize - 1))) };
             self.len -= 1;
             to_return
         }
@@ -170,5 +175,93 @@ impl<T> Deref for MVector<T> {
 impl<T> DerefMut for MVector<T> {
     fn deref_mut(&mut self) -> &mut [T] {
         unsafe { slice::from_raw_parts_mut(self.ptr.as_mut(), self.len) }
+    }
+}
+
+#[allow(unused)]
+pub struct MVectorIntoIter<T> {
+    buf: Unique<T>,
+    cap: usize,
+
+    start: *const T,
+    end: *const T,
+}
+
+impl<T> Iterator for MVectorIntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                let item = ptr::read(self.start);
+                self.start = self.start.offset(1);
+                Some(item)
+            }
+        }
+    }
+}
+
+impl<T> IntoIterator for MVector<T> {
+    type Item = T;
+    type IntoIter = MVectorIntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let ptr = self.ptr;
+        let cap = self.cap;
+        let len = self.len;
+
+        // Make sure not to drop Vec since that will free the buffer
+        // TODO: make clear what will happen if we forget to using forget.
+        mem::forget(self);
+
+        unsafe {
+            MVectorIntoIter {
+                buf: ptr,
+                cap,
+                start: ptr.as_ptr(),
+                // Note: handle the case when cap is zero, and Unique is just a fucking empty pointer.
+                end: if cap == 0 {
+                    // can't offset off this pointer, it's not allocated!
+                    ptr.as_ptr()
+                } else {
+                    ptr.as_ptr().offset(len as isize)
+                },
+            }
+        }
+    }
+
+    //    fn size_hint(&self) -> (usize, Option<usize>) {
+    //        let len = (self.end as usize - self.start as usize)
+    //            / mem::size_of::<T>();
+    //        (len, Some(len))
+    //    }
+}
+
+impl<T> DoubleEndedIterator for MVectorIntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                self.end = self.end.offset(-1);
+                Some(ptr::read(self.end))
+            }
+        }
+    }
+}
+
+impl<T> Drop for MVectorIntoIter<T> {
+    fn drop(&mut self) {
+        let align = mem::align_of::<T>();
+        let elem_sz = mem::size_of::<T>();
+
+        unsafe {
+            dealloc(
+                self.buf.as_ptr() as *mut _,
+                Layout::from_size_align_unchecked(elem_sz, align),
+            );
+        }
     }
 }
