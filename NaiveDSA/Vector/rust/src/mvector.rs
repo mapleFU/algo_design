f43b0c811f42cc4a5de6998b9dbcdecd13321c2f
mapleFU <1506118561@qq.com>
2019-11-07 02:25:56 +0800

    [ADD] Implement with a raw_vec

--- NaiveDSA/Vector/rust/src/mvector.rs
+++ NaiveDSA/Vector/rust/src/mvector.rs
@@ -1,7 +1,6 @@
-use std::alloc::{alloc, dealloc, realloc, Layout};
 use std::mem;
 use std::ops::{Deref, DerefMut};
-use std::ptr::Unique;
+use std::{ptr, slice};
 
 #[allow(unused)]
 use crate::raw_vec::*;
@@ -13,29 +12,28 @@ use crate::raw_vec::*;
 ///    https://opensource.apple.com/source/clang/clang-211.10.1/src/docs/LangRef.html#i_getelementptr
 ///    https://opensource.apple.com/source/clang/clang-211.10.1/src/docs/GetElementPtr.html#intro
 ///
-use std::{ptr, slice};
+
 
 pub struct MVector<T> {
-    ptr: Unique<T>,
-    cap: usize,
+    buf: MRawVec<T>,
     len: usize,
 }
 
 impl<T> MVector<T> {
     pub fn insert(&mut self, index: usize, elem: T) {
         assert!(index <= self.len, "index is out of bound");
-        if self.cap == self.len {
+        if self.buf.cap == self.len {
             self.grow();
         }
         unsafe {
             // insert at `index`, meaning that [index, end) will be moved to [index + 1, end + 1)
             // [index] = elem.
             ptr::copy(
-                self.ptr.as_ptr().offset(index as isize),
-                self.ptr.as_ptr().offset((index + 1) as isize),
+                self.buf.ptr.as_ptr().offset(index as isize),
+                self.buf.ptr.as_ptr().offset((index + 1) as isize),
                 self.len - index,
             );
-            ptr::write(self.ptr.as_ptr().offset(index as isize), elem);
+            ptr::write(self.buf.ptr.as_ptr().offset(index as isize), elem);
         }
         self.len += 1;
     }
@@ -49,10 +47,10 @@ impl<T> MVector<T> {
         assert!(index < self.len, "index must < self.len in `remove`");
         self.len -= 1;
         unsafe {
-            let to_return = ptr::read(self.ptr.as_ptr().offset(index as isize));
+            let to_return = ptr::read(self.buf.ptr.as_ptr().offset(index as isize));
             ptr::copy(
-                self.ptr.as_ptr().offset((index + 1) as isize),
-                self.ptr.as_ptr().offset(index as isize),
+                self.buf.ptr.as_ptr().offset((index + 1) as isize),
+                self.buf.ptr.as_ptr().offset(index as isize),
                 self.len - index,
             );
             to_return
@@ -60,11 +58,11 @@ impl<T> MVector<T> {
     }
 
     pub fn push(&mut self, elem: T) {
-        if self.len == self.cap {
+        if self.len == self.buf.cap {
             self.grow();
         }
         unsafe {
-            ptr::write(self.ptr.as_ptr().offset(self.len as isize), elem);
+            ptr::write(self.buf.ptr.as_ptr().offset(self.len as isize), elem);
         }
         self.len += 1;
     }
@@ -73,8 +71,11 @@ impl<T> MVector<T> {
         if self.len == 0 {
             None
         } else {
-            let to_return =
-                unsafe { Some(ptr::read(self.ptr.as_ptr().offset(self.len as isize - 1))) };
+            let to_return = unsafe {
+                Some(ptr::read(
+                    self.buf.ptr.as_ptr().offset(self.len as isize - 1),
+                ))
+            };
             self.len -= 1;
             to_return
         }
@@ -84,56 +85,13 @@ impl<T> MVector<T> {
         assert_ne!(mem::size_of::<T>(), 0);
         println!("new() called");
         MVector {
-            ptr: Unique::empty(),
-            cap: 0,
+            buf: MRawVec::new(),
             len: 0,
         }
     }
 
     fn grow(&mut self) {
-        unsafe {
-            let align = mem::align_of::<T>();
-            let elem_sz = mem::size_of::<T>();
-
-            let (new_cap, ptr) = if self.cap == 0 {
-                let ptr = alloc(Layout::from_size_align_unchecked(elem_sz, align));
-                (1, ptr)
-            } else {
-                // as an invariant, we can assume that `self.cap < isize::MAX`,
-                // so this doesn't need to be checked.
-                let new_cap = self.cap * 2;
-                // Similarly this can't overflow due to previously allocating this
-                let old_num_bytes = self.cap * elem_sz;
-
-                // check that the new allocation doesn't exceed `isize::MAX` at all
-                // regardless of the actual size of the capacity. This combines the
-                // `new_cap <= isize::MAX` and `new_num_bytes <= usize::MAX` checks
-                // we need to make. We lose the ability to allocate e.g. 2/3rds of
-                // the address space with a single Vec of i16's on 32-bit though.
-                // Alas, poor Yorick -- I knew him, Horatio.
-                assert!(
-                    old_num_bytes <= (::std::isize::MAX as usize) / 2,
-                    "capacity overflow"
-                );
-
-                let new_num_bytes = old_num_bytes * 2;
-
-                let ptr = realloc(
-                    self.ptr.as_ptr() as *mut _,
-                    Layout::from_size_align_unchecked(old_num_bytes, align),
-                    new_num_bytes,
-                );
-                (new_cap, ptr)
-            };
-
-            // If allocate or reallocate fail, we'll get `null` back
-            if ptr.is_null() {
-                panic!("oom");
-            }
-            // Unique must not be null
-            self.ptr = Unique::new(ptr as *mut _).unwrap();
-            self.cap = new_cap;
-        }
+        self.buf.grow();
     }
 
     pub fn len(&self) -> usize {
@@ -141,25 +99,25 @@ impl<T> MVector<T> {
     }
 
     pub fn cap(&self) -> usize {
-        self.cap
+        self.buf.cap
     }
 }
 
 impl<T> Drop for MVector<T> {
     fn drop(&mut self) {
-        if self.cap != 0 {
+        if self.buf.cap != 0 {
             // need to release memory
             while let Some(_) = self.pop() {}
-
-            let align = mem::align_of::<T>();
-            let elem_sz = mem::size_of::<T>();
-
-            unsafe {
-                dealloc(
-                    self.ptr.as_ptr() as *mut _,
-                    Layout::from_size_align_unchecked(elem_sz, align),
-                );
-            }
+            // Not need to self delete.
+            //            let align = mem::align_of::<T>();
+            //            let elem_sz = mem::size_of::<T>();
+            //
+            //            unsafe {
+            //                dealloc(
+            //                    self.ptr.as_ptr() as *mut _,
+            //                    Layout::from_size_align_unchecked(elem_sz, align),
+            //                );
+            //            }
         }
     }
 }
@@ -168,20 +126,19 @@ impl<T> Deref for MVector<T> {
     type Target = [T];
 
     fn deref(&self) -> &[T] {
-        unsafe { slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
+        unsafe { slice::from_raw_parts(self.buf.ptr.as_ptr(), self.len) }
     }
 }
 
 impl<T> DerefMut for MVector<T> {
     fn deref_mut(&mut self) -> &mut [T] {
-        unsafe { slice::from_raw_parts_mut(self.ptr.as_mut(), self.len) }
+        unsafe { slice::from_raw_parts_mut(self.buf.ptr.as_mut(), self.len) }
     }
 }
 
 #[allow(unused)]
 pub struct MVectorIntoIter<T> {
-    buf: Unique<T>,
-    cap: usize,
+    _buf: MRawVec<T>,
 
     start: *const T,
     end: *const T,
@@ -208,18 +165,24 @@ impl<T> IntoIterator for MVector<T> {
     type IntoIter = MVectorIntoIter<T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        let ptr = self.ptr;
-        let cap = self.cap;
-        let len = self.len;
 
-        // Make sure not to drop Vec since that will free the buffer
-        // TODO: make clear what will happen if we forget to using forget.
-        mem::forget(self);
+        // need to use ptr::read to unsafely move the buf out since it's
+        // not Copy, and Vec implements Drop (so we can't destructure it).
+        // Use but removed:
+        // ```
+        // let buf = self.buf;
+        // ```
 
         unsafe {
+            let buf = ptr::read(&self.buf);
+            let cap = buf.cap;
+            let len = self.len;
+
+            // Make sure not to drop Vec since that will free the buffer
+            // TODO: make clear what will happen if we forget to using forget.
+            mem::forget(self);
+            let ptr = buf.ptr;
             MVectorIntoIter {
-                buf: ptr,
-                cap,
                 start: ptr.as_ptr(),
                 // Note: handle the case when cap is zero, and Unique is just a fucking empty pointer.
                 end: if cap == 0 {
@@ -228,6 +191,7 @@ impl<T> IntoIterator for MVector<T> {
                 } else {
                     ptr.as_ptr().offset(len as isize)
                 },
+                _buf:  buf,
             }
         }
     }
@@ -252,16 +216,16 @@ impl<T> DoubleEndedIterator for MVectorIntoIter<T> {
     }
 }
 
-impl<T> Drop for MVectorIntoIter<T> {
-    fn drop(&mut self) {
-        let align = mem::align_of::<T>();
-        let elem_sz = mem::size_of::<T>();
-
-        unsafe {
-            dealloc(
-                self.buf.as_ptr() as *mut _,
-                Layout::from_size_align_unchecked(elem_sz, align),
-            );
-        }
-    }
-}
+//impl<T> Drop for MVectorIntoIter<T> {
+//    fn drop(&mut self) {
+//        let align = mem::align_of::<T>();
+//        let elem_sz = mem::size_of::<T>();
+//
+//        unsafe {
+//            dealloc(
+//                self.buf.as_ptr() as *mut _,
+//                Layout::from_size_align_unchecked(elem_sz, align),
+//            );
+//        }
+//    }
+//}
--- NaiveDSA/Vector/rust/src/raw_vec.rs
+++ NaiveDSA/Vector/rust/src/raw_vec.rs
@@ -1,1 +1,83 @@
+use std::alloc::{alloc, dealloc, realloc, Layout};
+use std::mem;
 use std::ptr::Unique;
+
+/// Now that we've implemented it and identified actual logic duplication,
+/// this is a good time to perform some logic compression.
+/// 我个人感觉比较类似 C++ 的 `vector_base`
+pub struct MRawVec<T> {
+    pub(crate) ptr: Unique<T>,
+    pub(crate) cap: usize,
+}
+
+impl<T> MRawVec<T> {
+    pub fn new() -> MRawVec<T> {
+        assert_ne!(mem::size_of::<T>(), 0, "TODO: implement ZST support");
+        MRawVec {
+            ptr: Unique::empty(),
+            cap: 0,
+        }
+    }
+
+    pub(crate) fn grow(&mut self) {
+        unsafe {
+            let align = mem::align_of::<T>();
+            let elem_sz = mem::size_of::<T>();
+
+            let (new_cap, ptr) = if self.cap == 0 {
+                let ptr = alloc(Layout::from_size_align_unchecked(elem_sz, align));
+                (1, ptr)
+            } else {
+                // as an invariant, we can assume that `self.cap < isize::MAX`,
+                // so this doesn't need to be checked.
+                let new_cap = self.cap * 2;
+                // Similarly this can't overflow due to previously allocating this
+                let old_num_bytes = self.cap * elem_sz;
+
+                // check that the new allocation doesn't exceed `isize::MAX` at all
+                // regardless of the actual size of the capacity. This combines the
+                // `new_cap <= isize::MAX` and `new_num_bytes <= usize::MAX` checks
+                // we need to make. We lose the ability to allocate e.g. 2/3rds of
+                // the address space with a single Vec of i16's on 32-bit though.
+                // Alas, poor Yorick -- I knew him, Horatio.
+                assert!(
+                    old_num_bytes <= (::std::isize::MAX as usize) / 2,
+                    "capacity overflow"
+                );
+
+                let new_num_bytes = old_num_bytes * 2;
+
+                let ptr = realloc(
+                    self.ptr.as_ptr() as *mut _,
+                    Layout::from_size_align_unchecked(old_num_bytes, align),
+                    new_num_bytes,
+                );
+                (new_cap, ptr)
+            };
+
+            // If allocate or reallocate fail, we'll get `null` back
+            if ptr.is_null() {
+                panic!("oom");
+            }
+            // Unique must not be null
+            self.ptr = Unique::new(ptr as *mut _).unwrap();
+            self.cap = new_cap;
+        }
+    }
+}
+
+impl<T> Drop for MRawVec<T> {
+    fn drop(&mut self) {
+        if self.cap != 0 {
+            let align = mem::align_of::<T>();
+            let elem_sz = mem::size_of::<T>();
+
+            unsafe {
+                dealloc(
+                    self.ptr.as_ptr() as *mut _,
+                    Layout::from_size_align_unchecked(elem_sz, align),
+                );
+            }
+        }
+    }
+}
