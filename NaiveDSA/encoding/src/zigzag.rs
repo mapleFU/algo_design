93d7b85df7c3da1475a70f950381bf67018765ac
mwish <1506118561@qq.com>
2019-12-01 01:24:32 +0800

    Dev zigzag (#1)
    * [ADD] adding contents for zigzag
    
    * [ADD] adding readme for zigzag
    
    * [ADD] fmt tests and change files from varint to zigzag
    
    * [ADD] implement zig-zag for signed-int
    
    * [ADD] adding varint for encoding
    
    * [ADD] Finish a varint encoding
    
    * [Update] stable the old interface
    
    * [ADD] using ::one() and ::zero() to simplify the code

---
+++ NaiveDSA/encoding/src/zigzag.rs
@@ -0,0 +1,47 @@
+/// ZigZag is an algorithm according to this note: https://gist.github.com/mfuerstenau/ba870a29e16536fdbaba
+/// It was common used by some rpc protocol, like protobuf and avro.
+use num_traits::int;
+use num_traits::CheckedNeg;
+use std::mem;
+
+pub trait ZigZag {
+    fn zig_encoding(self) -> Self;
+    fn zig_decoding(self) -> Self;
+}
+
+impl<T: int::PrimInt + CheckedNeg> ZigZag for T {
+    fn zig_encoding(self) -> Self {
+        // zigzag for integer
+        let sz: usize = mem::size_of::<Self>() * 8;
+        (self << 1) ^ (self >> (sz - 1))
+    }
+
+    fn zig_decoding(self) -> Self {
+        self.unsigned_shr(1) ^ self.bitand(Self::one()).checked_neg().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ZigZag;
+
+    #[test]
+    fn test_encoding() {
+        for i in -100..=100 {
+            if i < 0 {
+                assert_eq!(-i * 2 - 1, i.zig_encoding());
+            } else {
+                // i >= 0
+                assert_eq!(i * 2, i.zig_encoding());
+            }
+        }
+    }
+
+    #[test]
+    fn test_decoding() {
+        for i in -100..=100 {
+            let v = i.zig_encoding();
+            assert_eq!(i, v.zig_decoding());
+        }
+    }
+}
