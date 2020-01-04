22cc3651d54b5f8ef7d5502c8f2a024223670bf2
mapleFU <1506118561@qq.com>
2020-01-04 18:48:51 +0800

    [Amend] amend testing for Stack

--- Concurrency/rust-port/src/stack.rs
+++ Concurrency/rust-port/src/stack.rs
@@ -1,5 +1,5 @@
 use std::collections::LinkedList;
-use std::sync::{Arc, Condvar, Mutex, MutexGuard};
+use std::sync::{Condvar, Mutex, MutexGuard};
 
 /// `Stack` is a trait for stack, user can push pop in this stack.
 pub trait Stack<T> {
@@ -22,7 +22,7 @@ macro_rules! impl_sync {
 
 #[derive(Default)]
 pub struct LockStack<T> {
-    guard: Mutex<LinkedList<T>>,
+    pub(crate) guard: Mutex<LinkedList<T>>,
 }
 
 impl_sync!(LockStack<T>);
@@ -52,7 +52,7 @@ impl<T: Copy> Stack<T> for LockStack<T> {
 
 #[derive(Default)]
 pub struct CondStack<T> {
-    guard: Arc<Mutex<LinkedList<T>>>,
+    guard: LockStack<T>,
     cond: Condvar,
 }
 
@@ -60,28 +60,20 @@ impl_sync!(CondStack<T>);
 
 impl<T: Copy> Stack<T> for CondStack<T> {
     fn push(&self, obj: T) {
-        let mut guard = self.guard.lock().unwrap();
-        guard.push_back(obj);
+        self.guard.push(obj);
         self.cond.notify_one();
     }
 
     fn empty(&self) -> bool {
-        self.guard.lock().unwrap().is_empty()
+        self.guard.empty()
     }
 
     fn pop(&self) -> Option<T> {
-        let mut guard = self.guard.lock().unwrap();
-        match guard.back() {
-            Some(&s) => {
-                guard.pop_back();
-                Some(s)
-            }
-            None => None,
-        }
+        self.guard.pop()
     }
 
     fn pop_wait(&self) -> T {
-        let result = self.guard.lock();
+        let result = self.guard.guard.lock();
         let mut entity: MutexGuard<LinkedList<T>> = result.unwrap();
         if !entity.is_empty() {
             entity.pop_back().unwrap()
@@ -93,3 +85,68 @@ impl<T: Copy> Stack<T> for CondStack<T> {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use crate::stack::{CondStack, LockStack, Stack};
+    use std::sync::Arc;
+    use std::thread::spawn;
+
+    fn concurrency_get_test<T: Stack<i32> + Default + Sync + Send + 'static>() {
+        let s1 = T::default();
+        let cnt_size = 10000;
+        for i in 0..cnt_size {
+            s1.push(i);
+        }
+
+        let arc = Arc::new(s1);
+        let mut tasks = vec![];
+        for _ in 0..5 {
+            let stack = arc.clone();
+            tasks.push(spawn(move || {
+                let mut cnt = 0;
+                while !stack.empty() {
+                    if stack.pop().is_some() {
+                        cnt += 1;
+                    }
+                }
+                cnt
+            }));
+        }
+
+        assert_eq!(cnt_size, tasks.into_iter().map(|t| t.join().unwrap()).sum());
+    }
+
+    #[test]
+    fn test_stack_1() {
+        concurrency_get_test::<LockStack<i32>>();
+    }
+
+    #[test]
+    fn test_stack_2() {
+        concurrency_get_test::<CondStack<i32>>();
+    }
+
+    #[test]
+    fn test_cond_wait() {
+        let cond = CondStack::default();
+        let mut task_vec = vec![];
+        let arc_cond = Arc::new(cond);
+        let task_cnt = 10;
+        for _ in 0..task_cnt {
+            let cond_t = arc_cond.clone();
+            task_vec.push(spawn(move || cond_t.pop_wait()));
+        }
+
+        for i in 0..task_cnt {
+            arc_cond.push(i);
+        }
+
+        let mut v: Vec<i32> = task_vec.into_iter().map(|t| t.join().unwrap()).collect();
+        v.sort();
+
+        assert_eq!(v.len() as i32, task_cnt);
+        for ta in 0..task_cnt {
+            assert_eq!(ta, v[ta as usize]);
+        }
+    }
+}
